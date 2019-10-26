55e14bb9d5566ec6703805ac0de4c4c80b78ac1c
bowen <xianxueniao150>
2019-10-26 14:42:42 +0800

    add cookie

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -2,12 +2,19 @@ package life.majiang.community.community.controller;
 
 import life.majiang.community.community.dto.AccessTokenDto;
 import life.majiang.community.community.dto.GithubUser;
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.User;
 import life.majiang.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,10 +31,15 @@ public class AuthorizeController {
     @Value("${github.client.uri}")
     private String clientUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setCode(code);
@@ -35,9 +47,20 @@ public class AuthorizeController {
         accessTokenDto.setState(state);
         accessTokenDto.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getLogin());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/life/majiang/community/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -2,6 +2,25 @@ package life.majiang.community.community.dto;
 
 public class GithubUser {
     private String name;
+    private String login;
+
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     private long id;
 
     public String getName() {
